76c6c0d943a1da8f948c0451dd10c23b6c5a6847
Toni Tralice <atralice@gmail.com>
2018-07-27 19:42:12 -0300

    reorganized folder structure

--- start.sh
+++ start.sh
@@ -4,42 +4,47 @@ enviroment=${1:-'none'}
 action=$2
 api=${3:-'none'}
 testName=${4:-'none'}
-rm .env
-touch .env
-cat ./config/env/.envBase > .env
+rm ./config/.env
+touch ./config/.env
+cat ./config/env/.envBase > ./config/.env
 if [ $enviroment = 'dev' ]
   then
     if [ $api = 'api' ]
       then
         echo 'starting DEVELOPMENT API only:'
-        cat ./config/env/.envDev >> ./.env
+        cat ./config/env/.envDev >> ./config/.env
+        cd ./config
         docker-compose -f docker-compose-api.yaml $action
     elif [ $api = 'test' ]
       then
         echo 'starting TESTING enviroment:'
-        cat ./config/env/.envTest >> ./.env
+        cat ./config/env/.envTest >> ./config/.env
         if [ $testName != 'none' ]
           then
             echo $testName
-            echo CMD=npm run test ./tests/**/$testName.spec.js >>  ./.env
+            echo CMD=npm run test ./tests/**/$testName.spec.js >>  ./config/.env
           else
-            echo 'CMD=npm run test ./tests/**/*.spec.js' >>  ./.env
+            echo 'CMD=npm run test ./tests/**/*.spec.js' >>  ./config/.env
         fi
+        cd ./config
         docker-compose -f docker-compose-api.yaml $action pg redis api
     elif [ $api = 'cosmos' ]
       then
         echo 'starting COSMOS enviroment:'
-        cat ./config/env/.envCosmos >> ./.env
+        cat ./config/env/.envCosmos >> ./config/.env
+        cd config
         docker-compose -f docker-compose-cosmos.yaml $action app
     else
       echo 'starting DEVELOPMENT:'
-      cat ./config/env/.envDev >> ./.env
+      cat ./config/env/.envDev >> ./config/.env
+      cd ./config
       docker-compose -f docker-compose.yaml $action
     fi
 elif [ $enviroment = 'production' ]
   then
     echo 'starting PRODUCTION:'
-    cat ./config/env/.envProd >> ./.env
+    cat ./config/env/.envProd >> ./config/.env
+    cd config
     docker-compose -f docker-compose-prod.yaml $action
 else
     echo 'incorrect enviroment. Choose "production" or "dev"'
